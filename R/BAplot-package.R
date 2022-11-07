bb45eb851e40930257eeab603764567563ba1d0c
Konstantin Lang <konstantin.lang1@bayer.com>
2022-11-07 16:16:45 +0100

    added `globalVariables`

--- R/BAplot-package.R
+++ R/BAplot-package.R
@@ -5,3 +5,7 @@
 #' @importFrom lifecycle deprecated
 ## usethis namespace: end
 NULL
+utils::globalVariables(c("parameter", "avg", "dfce", ".", "value", "xmx", "xmn", "ymx", "ymn",
+                         "dplac", "ltyp", "lsiz", "tx", "ty", "tlbl"))
+utils::globalVariables(c("dfce", "conf.int", "n", "stdv", "bias", "bias.add", "lloa", "loa.add",
+                         "uloa", "uloa.ucl"))
